84f87bf43f01cfd0160a53237c803cac7e86681c
swapna-321 <swapna@gmail.com>
2024-08-12 23:11:26 +0530

    threads

--- Synchronizate.java
+++ Synchronizate.java
@@ -1,3 +1,46 @@
 public class Synchronizate{
-  // Show The Difference Between Synchronized and static synchronized and print both functions
+// Synchronized method (locks on the instance)
+// Show The Difference Between Synchronized and static synchronized and print both functionslocks on the instance)
+  
+  public synchronized void instanceMethod() {
+      for (int i = 0; i < 5; i++) {
+          System.out.println("Instance method - " + i);
+          try {
+              Thread.sleep(100); // Pause to simulate work
+          } catch (InterruptedException e) {
+              e.printStackTrace();
+          }
+      }
+  }
+
+  // Static synchronized method (locks on the class)
+  public static synchronized void staticMethod() {
+      for (int i = 0; i < 5; i++) {
+          System.out.println("Static method - " + i);
+          try {
+              Thread.sleep(100); // Pause to simulate work
+          } catch (InterruptedException e) {
+              e.printStackTrace();
+          }
+      }
+  }
+
+  public static void main(String[] args) {
+      // Create an instance of the Synchronizate class
+      Synchronizate obj = new Synchronizate();
+
+      // Thread for instanceMethod
+      new Thread(() -> obj.instanceMethod()).start();
+
+      // Another thread for instanceMethod
+      new Thread(() -> obj.instanceMethod()).start();
+
+      // Thread for staticMethod
+      new Thread(() -> Synchronizate.staticMethod()).start();
+
+      // Another thread for staticMethod
+      new Thread(() -> Synchronizate.staticMethod()).start();
+  }
 }
+
+
