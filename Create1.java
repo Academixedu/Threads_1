76f70bd9464e5e52d5237cf274589f11224426f4
Chaitanyabeeraka11 <chaitanyabeeraka@gmail.com>
2024-08-12 20:39:55 +0530

    Done

--- Create1.java
+++ Create1.java
@@ -1,3 +1,15 @@
-public class Create1{
-// implement Runnable and Post The Sub Class Object in to Thread and run the Logic to print Threadname for 10 times
+public class Create1 implements Runnable {
+    @Override
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(Thread.currentThread().getName() + " " + i);
+        }
+    }
+
+    public static void main(String[] args) {
+        Create1 create1 = new Create1();
+        Thread thread = new Thread(create1);
+        thread.setName("Thread1");
+        thread.start();
+    }
 }
