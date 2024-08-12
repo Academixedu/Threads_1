30b28a42f28ea69309c4d16a96b95ff96621422b
Your Name <your@email.com>
2024-08-12 19:58:55 +0530

    Hemanth

--- Create1.java
+++ Create1.java
@@ -1,3 +1,15 @@
-public class Create1{
-// implement Runnable and Post The Sub Class Object in to Thread and run the Logic to print Threadname for 10 times
+public class Create1 {
+    public static void main(String[] args) {
+        Runnable task = new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 10; i++) {
+                    System.out.println(Thread.currentThread().getName() + " Count: " + i);
+                }
+            }
+        };
+        Thread t = new Thread(task);
+        t.setName("Thread");
+        t.start();
+    }
 }
