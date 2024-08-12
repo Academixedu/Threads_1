3592f1e1757958192e65c736a69b0fdd43f0f028
MekaManojKrishnalakshman <mekamanojkrishnalakshman@gmail.com>
2024-08-12 19:52:10 +0530

    done

--- Create1.java
+++ Create1.java
@@ -1,3 +1,31 @@
 public class Create1{
+    public class Create implements Runnable{
+        public void run(){
+            for(int i=1;i<=10;i++){
+                System.out.println(Thread.currentThread().getName());
+            }
+        }
+
+        public static void main(String[] args) {
+            Create1 de=new Create1();
+            Create t1=de.new Create();
+            Thread t=new Thread(t1);
+            t.start();
+            
+           
+
+        
+
+          
+    }
+
+      
+         
+
+    
+
 // implement Runnable and Post The Sub Class Object in to Thread and run the Logic to print Threadname for 10 times
+}
+
+
 }
