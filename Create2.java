411fcafaf6a36b7092f85fa44b09f469bca5acb8
siva-245711 <sivanagendra809@gmail.com>
2024-08-12 20:33:03 +0530

    threds---

--- Create2.java
+++ Create2.java
@@ -1,3 +1,18 @@
 public class Create2{
 // Prepare a Thread Object and Override run() method in it using Anomous class Approach
+
+    public static void main(String[] args) {
+        Thread t = new Thread() {
+            public void run() {
+                for (int i = 0; i < 5; i++) {
+                    System.out.println("hi threds");
+                }
+            }
+        };
+        t.start();
+        for (int i = 0; i < 5; i++) {
+            System.out.println(Thread.currentThread().getName());
+        }
+}
+
 }
--- Synchronizate.java
+++ Synchronizate.java
@@ -1,3 +1,32 @@
 public class Synchronizate{
   // Show The Difference Between Synchronized and static synchronized and print both functions
+  public synchronized void m1(){
+    for(int i=0;i<5;i++){
+      System.out.println("synchronized"+Thread.currentThread().getName());
+    }
+  }
+
+  public static synchronized void m2(){
+    for(int i=0;i<5;i++){
+      System.out.println("Static synchronized"+Thread.currentThread().getName());
+    }
+  }
+
+  public static void main(String[] args) {
+    Synchronizate s = new Synchronizate();
+    Thread t = new Thread(){
+    public void run(){
+      s.m1();
+    }
+  };
+  t.start();
+    Synchronizate s1 = new Synchronizate();
+    Thread t1 = new Thread(){
+    public void run(){
+      s1.m1();
+    }
+  };
+  t1.start();
+
+  }
 }
