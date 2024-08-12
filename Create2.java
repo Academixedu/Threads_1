2f93d5ad9d3d74afbf92f52b0a4f0eaba0869f98
manicharanchindam <manicharanchindam@email.com>
2024-08-12 22:33:01 +0530

    task done

--- Create2.java
+++ Create2.java
@@ -1,3 +1,15 @@
 public class Create2{
-// Prepare a Thread Object and Override run() method in it using Anomous class Approach
-}
+    
+        public static void main(String args[]){
+            Thread thread=new Thread(new Runnable(){
+                public void run(){
+                    for(int i=1;i<=10;i++){
+                        System.out.println(Thread.currentThread().getName()+" "+i+"..");
+                    }
+                }
+            });
+    
+            thread.setName("Thread ");
+            thread.start();
+        }
+    }
