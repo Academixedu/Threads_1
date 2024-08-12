bda97a343a56697e7439c413da837e05f501acfe
urstrulysmohana <urstrulysmohan@gmail.com>
2024-08-12 20:44:08 +0530

    threads

--- Create2.java
+++ Create2.java
@@ -1,3 +1,14 @@
-public class Create2{
-// Prepare a Thread Object and Override run() method in it using Anomous class Approach
+public class Create2 {
+    // Prepare a Thread Object and Override run() method in it using Anomous class Approach
+    public static void main(String[] args) {
+        Thread thread = new Thread() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 10; i++) {
+                    System.out.println(Thread.currentThread().getName() + " - Count: " + i);
+                }
+            }
+        };
+        thread.start();
+    }
 }
--- Synchronizate.java
+++ Synchronizate.java
@@ -1,3 +1,57 @@
-public class Synchronizate{
-  // Show The Difference Between Synchronized and static synchronized and print both functions
+public class Synchronizate {
+
+  public synchronized void synchronizedMethod() {
+      for (int i = 0; i < 10; i++) {
+          System.out.println("synchronized - Count: " + i);
+          try {
+              Thread.sleep(100);
+          } catch (InterruptedException e) {
+              e.printStackTrace();
+          }
+      }
+  }
+
+  public static synchronized void staticSynchronizedMethod() {
+      for (int i = 0; i < 10; i++) {
+          System.out.println("static synchronized - Count: " + i);
+          try {
+              Thread.sleep(100);
+          } catch (InterruptedException e) {
+              e.printStackTrace();
+          }
+      }
+  }
+
+  public static void main(String[] args) {
+      Synchronizate obj = new Synchronizate();
+
+      // Non-static synchronized method
+      Thread t1 = new Thread(new Runnable() {
+          @Override
+          public void run() {
+              obj.synchronizedMethod();
+          }
+      });
+
+      // Static synchronized method
+      Thread t2 = new Thread(new Runnable() {
+          @Override
+          public void run() {
+              staticSynchronizedMethod();
+          }
+      });
+
+      // Start the first thread
+      t1.start();
+
+      try {
+          // Wait for t1 to finish before starting t2
+          t1.join();
+      } catch (InterruptedException e) {
+          e.printStackTrace();
+      }
+
+      // Start the second thread after the first one has finished
+      t2.start();
+  }
 }
