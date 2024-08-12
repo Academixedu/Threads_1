dfb8a9163893c07e9dd1ac432b76601950c88d0d
dharmapuriyudhveer <dharmapuriyudhveer@gmail.com>
2024-08-12 19:56:56 +0530

    Threads

--- Create1.java
+++ Create1.java
@@ -1,3 +1,19 @@
-public class Create1{
+public class Create1 implements Runnable{
+
+    @Override
+    public void run() 
+    {
+        for(int i=0;i<10;i++)
+        {
+            System.out.println(Thread.currentThread().getName());
+        }
+    }
 // implement Runnable and Post The Sub Class Object in to Thread and run the Logic to print Threadname for 10 times
+
+    public static void main(String[] args)
+    {
+        Create1 t=new Create1();
+        Thread t1=new Thread(t);
+        t1.start();
+    }
 }
