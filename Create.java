5e40f098751205a35984abf81f1d78bc262fe7ff
Bharathmanne18 <bharathmanne19@gmail.com>
2024-08-20 18:42:23 +0530

    Update Create.java

--- Create.java
+++ Create.java
@@ -1,3 +1,13 @@
-public class Create extends Thread{
-// Prepare a Logic to print Thread name  for 10 times using Thread Concept
-}
+ public class Create extends Thread{
+    // Prepare a Logic to print Thread name  for 10 times using Thread Concept
+    public void run(){
+        for(int i=1;i<=10;i++){
+        System.out.println("Thread"+i);
+        }
+    }
+    public static void main(String[] args) {
+        Create c1=new Create();
+        c1.start();
+          }
+    }
+    
