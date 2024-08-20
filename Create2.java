328a82d043d8c158be58d4400d3b5aae0c891267
kavitha-guntru <kavithaguntru60@gmail.com>
2024-08-20 18:22:37 +0530

    kavitha

--- Create2.java
+++ Create2.java
@@ -1,3 +1,17 @@
-public class Create2{
+public class Create2 {
 // Prepare a Thread Object and Override run() method in it using Anomous class Approach
+public static void k1(){
+    for(int i=0;i<10;i++){
+        System.out.println(Thread.currentThread().getName()+" "+i);
+    }
+}
+public static void main(String[] args) {
+    Thread k = new Thread(){
+        @Override
+        public void run(){
+            k1();
+        }
+    };
+    k.start();
+}
 }
