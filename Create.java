cc476dd112efab1a022a523139bf6fe629276163
Verat-hub <venkey482thalka@gmail.com>
2024-08-20 19:07:38 +0530

    Venkat

--- Create.java
+++ Create.java
@@ -1,3 +1,12 @@
 public class Create extends Thread{
 // Prepare a Logic to print Thread name  for 10 times using Thread Concept
+public void run(){
+    for(int i=0;i<10;i++){
+        System.out.println(i+" "+Thread.currentThread().getName());
+    }
+}
+public static void main(String[] args) {
+    Create Thr= new Create();
+    Thr.start();
+}
 }
--- Create2.java
+++ Create2.java
@@ -1,3 +1,16 @@
 public class Create2{
 // Prepare a Thread Object and Override run() method in it using Anomous class Approach
+public static void A1(){
+    for(int i=0;i<=10;i++){
+        System.out.println(Thread.currentThread().getName());
+    }
+}
+public static void main(String[] args) {
+    Thread T = new Thread(){
+        public void run(){
+            A1();
+        }
+    };
+    T.start();
+}
 }
