35af1fda287d1547c5378ebfa6b9b79b207138c1
psvsai <pasupuletisvsm245@gmail.com>
2024-08-20 18:23:42 +0530

    psvsai

--- Create.java
+++ Create.java
@@ -1,3 +1,12 @@
 public class Create extends Thread{
 // Prepare a Logic to print Thread name  for 10 times using Thread Concept
+        public void run(){
+            for (int i=1; i<=10; i++){
+                System.out.println(Thread.currentThread().getName() + " " + i);
+            }
+        }
+        public static void main(String[] args) {
+            Create p = new Create();
+            p.start();
+        }
 }
