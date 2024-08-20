78bcd58c95ddda03ee76d5a85d3ab793b8fe7ade
Manisugguna0 <subrahmanyamanikanta@gmail.com>
2024-08-20 17:30:17 +0530

    MANI

--- Create.java
+++ Create.java
@@ -1,3 +1,15 @@
 public class Create extends Thread{
 // Prepare a Logic to print Thread name  for 10 times using Thread Concept
+@Override
+public void run(){
+    for(int i=0 ;i<=10;i++)
+    {
+        System.out.println(Thread.currentThread().getName()+" "+i);
+    }
+}
+
+public static void main(String[] args) {
+    Create c = new Create();
+    c.start();
+}
 }
