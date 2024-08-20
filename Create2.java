d1cb2b21431c16a4ecfb495ef626f023d02f3192
Bharathmanne18 <bharathmanne19@gmail.com>
2024-08-20 18:44:18 +0530

    Update Create2.java

--- Create2.java
+++ Create2.java
@@ -1,3 +1,18 @@
-public class Create2{
-// Prepare a Thread Object and Override run() method in it using Anomous class Approach
-}
+ public class Create2{
+    // Prepare a Thread Object and Override run() method in it using Anomous class Approach
+    public static void s(){
+        for(int i=1;i<=10;i++){
+            System.out.println("Thread2:"+i);
+        }
+    }
+    public static void main(String[] args) {
+        Thread t1= new Thread(){
+            @Override
+            public void run(){
+        s();
+
+            }
+        };
+        t1.start();
+    }
+    }
