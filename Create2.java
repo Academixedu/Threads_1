0494976f8cd8d7aebfb4666f753e2722f9a4186b
jangamswarupa-595 <swarupajangam9@gmail.com>
2024-08-20 18:03:43 +0530

    swarupa

--- Create2.java
+++ Create2.java
@@ -1,3 +1,19 @@
 public class Create2{
 // Prepare a Thread Object and Override run() method in it using Anomous class Approach
+public static void s(){
+  for(int i=0;i<10;i++){
+    System.out.println("Thread3:" +i);
+  }
+}
+public static void main(String[] args) {
+    Thread t1=new Thread(){
+        @Override
+        public void run()
+        {
+            s();
+
+        }
+    };
+    t1.start();
+}
 }
