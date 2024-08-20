e44bed0716c8352ba35f33485aac2ef3c50b4e95
saikumar34892 <saikumarmatta3821@gmail.com>
2024-08-20 17:35:06 +0530

    from sai

--- Create.java
+++ Create.java
@@ -1,3 +1,12 @@
 public class Create extends Thread{
 // Prepare a Logic to print Thread name  for 10 times using Thread Concept
+public void run(){
+    for(int i=1;i<=10;i++){
+        System.out.println(Thread.currentThread().getName());
+    }
+}
+public static void main(String[] args) {
+    Create c=new Create();
+    c.start(); 
+}
 }
