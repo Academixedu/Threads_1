07d40fe7c34b600d64e110ea1801de2ff2e93034
nayeem6836 <nayeemmaseed9100@gmail.com>
2024-08-20 19:14:28 +0530

    commit

--- Create1.java
+++ Create1.java
@@ -1,3 +1,16 @@
 public class Create1{
-// implement Runnable and Post The Sub Class Object in to Thread and run the Logic to print Threadname for 10 times
+    public class Create1 implements Runnable{
+    // implement Runnable and Post The Sub Class Object in to Thread and run the Logic to print Threadname for 10 times
+        public void run(){
+            for(int i=1;i<=10;i++){
+                System.out.println(i+"thread name is :"+Thread.currentThread().getName());
+            }
+        }
+    public static void main(String[] args) {
+        Create1 cr = new Create1();
+        Thread Th = new Thread(cr);
+        Th.start();
+    
+    }
+    }
 }
