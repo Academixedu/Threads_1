6f5170fac6023adfb0fb473b19bfcc00796200c4
syamkuamrnallamilli0 <syamkumarnallamilli0@gmail.com>
2024-08-21 14:59:46 +0530

    done

--- Create.java
+++ Create.java
@@ -1,3 +1,21 @@
 public class Create extends Thread{
 // Prepare a Logic to print Thread name  for 10 times using Thread Concept
+
+    @Override
+    public void run() {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("hello " +i+ " "  +Thread.currentThread().getName());
+        }
+    }
+
+    public static void main(String[] args) {
+        Create thread1 = new Create();//object//
+        Create thread2 = new Create();
+        
+        
+        thread1.start();
+        thread2.start();
+    }
 }
+
+
--- Create1.java
+++ Create1.java
@@ -1,3 +1,17 @@
-public class Create1{
+public class Create1 implements Runnable{
 // implement Runnable and Post The Sub Class Object in to Thread and run the Logic to print Threadname for 10 times
+@Override
+    public  void run(){
+        for(int i=0;i<=10;i++){
+            System.out.println("run "  +i+ " "  +Thread.currentThread().getName());
+        }
+    }
+    public static void main(String[] args) {
+        Create1 r1=new Create1();
+        Thread t=new Thread(r1);
+        Thread t2=new Thread(r1);
+        t.start();
+        t2.start();
+
+    }
 }
