dc281cf0e9e1a1b033a40b5a3d0953d74668c168
SriSankeerthanaGorrela <sankeerthanagorrela@gmail.com>
2024-08-21 14:51:51 +0530

    thread

--- Create1.java
+++ Create1.java
@@ -1,3 +1,13 @@
-public class Create1{
+public class Create1 implements Runnable{
 // implement Runnable and Post The Sub Class Object in to Thread and run the Logic to print Threadname for 10 times
-}
+public void run(){
+    for(int i=1;i<=10;i++){
+    System.out.println("Hello "+i+ " " +Thread.currentThread().getName());
+}}
+public static void main(String args[]){
+    Create1 m1=new Create1();//object
+    Thread t=new Thread(m1);//thread creation//
+    Thread t1=new Thread(m1);
+    t.start();
+    t1.start();
+}}
--- Create2.java
+++ Create2.java
@@ -1,3 +1,23 @@
 public class Create2{
 // Prepare a Thread Object and Override run() method in it using Anomous class Approach
+public static void m1(){
+    for(int i=1;i<=5;i++){
+        System.out.println("Hello "+i+ " " +Thread.currentThread().getName());
+
+    }
+}
+public static void main(String[] args) {
+    Thread t1=new Thread(){
+        @Override
+        public void run(){
+         m1();
+        }
+    }; t1.start();
+    Thread t2=new Thread(){
+        @Override
+        public void run(){
+         m1();
+        }
+    }; t2.start();
+}
 }
